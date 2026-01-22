7260074ab9fe4ac93318840833354e5b576d2b3f
ryznxx <ryuzenfarizi@gmail.com>
2026-01-23 02:35:08 +0700

    needed

--- sso/src/routes/+layout.svelte
+++ sso/src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import '../app.css';
+	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
-  export const ssr = false;
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
